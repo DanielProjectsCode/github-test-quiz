d57d163c7a4498e0831f01417f2d905dc9df23b1
BP1\8800at <daniel.vass@bp.com>
2022-03-16 14:23:16 +0000

    first

--- quiz/src/places/pages/NewPlace.js
+++ quiz/src/places/pages/NewPlace.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React from 'react';
 
 const NewPlace = () => {
-  return <h2>New place works</h2>;
+  return <h2>NewPlace Works!</h2>
 };
+
 export default NewPlace;
